11dc34b9d6ea3b8f0c4d40befba932c2847ae530
100-Han <cho.ooh.001@gmail.com>
2025-06-10 09:08:47 +0900

    도서관에서 쓸 도서관리 REST API 구현

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bookRouter = require('./routes/book');
 
 app.use(express.json());
+
 app.use('/books', bookRouter);
 app.use((req, res, next) => {
     res.status(404).json({error: '유효하지 않은 페이지 입니다.'});
